886e94a4e4d9c3800927be3756f1f1fb4d4c0427
i.kłusek <J6ZkqwVUn>
2023-02-11 22:17:49 +0100

    API: add commets and respones | mongoDB document schema | server config

---
+++ src/services/comments.services.ts
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose';
+import CommentsModel, { CommentInterface, commentSchema } from '../model/comments';
+
+async function saveComment(
+    commentTree: CommentInterface
+): Promise<{ status: number; message: string; commentId?: string; reason?: unknown }> {
+    try {
+        const { commentIndent } = commentTree;
+        if (commentIndent.level === 0) {
+            //save as a new comment with indent = 0 (main comment)
+            const newMainComment = new CommentsModel(commentTree);
+            const result = await newMainComment.save();
+            return { status: 201, message: 'Comment added as parent comment', commentId: result._id };
+        } else {
+            const isExists = await CommentsModel.findOne({
+                _id: new mongoose.Types.ObjectId(commentIndent.commentIds[0]),
+            }).exec();
+            if (!isExists) return { status: 404, message: 'Could not find given comment' };
+
+            //add another indent comment
+            // commentSchema.add({ responses: [commentSchema] }); // recursive
+
+            if (commentIndent.level === 1) {
+                console.log(commentTree);
+                console.log('level 1');
+                await CommentsModel.findOneAndUpdate(
+                    { _id: new mongoose.Types.ObjectId(commentIndent.commentIds[0]) },
+                    {
+                        $push: {
+                            responses: commentTree,
+                        },
+                    }
+                );
+            } else {
+                console.log('level 2');
+                console.log(commentIndent.commentIds[0], commentIndent.commentIds[1]);
+                const result = await CommentsModel.updateOne(
+                    { _id: new mongoose.Types.ObjectId(commentIndent.commentIds[0]) },
+                    {
+                        $push: {
+                            'responses.$[comment].responses': commentTree,
+                        },
+                    },
+                    {
+                        arrayFilters: [
+                            {
+                                'comment._id': new mongoose.Types.ObjectId(commentIndent.commentIds[1]),
+                            },
+                        ],
+                    }
+                );
+                console.log(result);
+            }
+
+            return { status: 201, message: `Comment added as child comment, indent: ${commentIndent.level}` };
+        }
+    } catch (err) {
+        return { status: 500, message: 'Could not save Item', reason: err };
+    }
+}
+
+export default { saveComment };
