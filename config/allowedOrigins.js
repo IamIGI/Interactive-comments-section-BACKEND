792870f0e9e2047eb715206b35b9090801a6aa09
i.kłusek <J6ZkqwVUn>
2023-02-23 16:33:46 +0100

    add to cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4173', //VITE - prod
+    'https://comment-section.iamigi.com/',
 ];
 exports.default = allowedOrigins;
