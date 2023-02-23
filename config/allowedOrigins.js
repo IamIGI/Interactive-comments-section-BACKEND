ec2a06deda52f1c5e5fa8b286f710aabec768db3
i.kłusek <J6ZkqwVUn>
2023-02-23 16:34:01 +0100

    add to cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, '__esModule', { value: true });
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4173', //VITE - prod
-    'https://comment-section.iamigi.com/',
+    'https://comment-section.iamigi.com',
 ];
 exports.default = allowedOrigins;
